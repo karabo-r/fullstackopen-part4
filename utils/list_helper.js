e477ed86fa4461cb150532830731cce36c775ab7
Karabo <karaborabothata0@gmail.com>
2022-07-25 10:21:57 +0200

    exercise 4.4 complete

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,18 @@
 const dummy = (blogs) => {
-    return 1
-  }
-  
-  module.exports = {
-    dummy
-  }
+	return 1;
+};
+
+const totalLikes = (blogs) => {
+	let numberOfLikes = [];
+	blogs.forEach((element) => {
+		numberOfLikes.push(element.likes);
+	});
+	const totalNumberOfLikes = numberOfLikes.reduce((sum, item) => {
+		return sum + item;
+	}, 0);
+	return totalNumberOfLikes;
+};
+module.exports = {
+	dummy,
+	totalLikes,
+};
