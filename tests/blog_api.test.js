4bd2cf2fe2ac5629c96a8b3db633d3d71a64f00a
Karabo <karaborabothata0@gmail.com>
2022-07-26 11:34:23 +0200

    exercise 4.9 complete

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 const supertest = require("supertest");
 const Blog = require("../models/blog");
 const app = require("../app");
-// const blog = require('../models/blog')
 
 const api = supertest(app);
 
 const initialBlogs = [
 	{
+		title: "The great war of 1812",
 		author: "John Smith",
-		subject: "The great war of 1812",
+		url: "url example",
 		likes: 43,
 	},
 ];
@@ -18,16 +18,22 @@ beforeEach(async () => {
 	await Blog.deleteMany({});
 	let newBlog = new Blog(initialBlogs[0]);
 	await newBlog.save();
-});
+}, 10000);
 
 test("returns the correct amount of blog posts in the JSON format", async () => {
 	await api.get("/api/blogs")
     .expect(200)
     .expect('Content-type', /application\/json/)
-    
+
     const response = await api.get('/api/blogs')
     expect(response.body).toHaveLength(initialBlogs.length)
 }, 10000);
+
+test('Verifying the existence of id property', async ()=>{
+	const response = await api.get('/api/blogs')
+	const blogId = response.body[0].id
+	expect(blogId).toBeDefined()
+}, 10000)
 
 afterAll(() => {
 	mongoose.connection.close();
