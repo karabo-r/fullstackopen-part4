acfd4fe626ce9b640705917b7fd7fbd195f7ca99
Karabo <karaborabothata0@gmail.com>
2022-07-29 19:20:10 +0200

    tests for empty username or password

--- models/user.js
+++ models/user.js
@@ -1,18 +1,42 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    name: String,
-    password: String
-})
+	username: {
+		type: String,
+		minLength: 3,
+		unique: [true, "make the username unique"],
+		required: [true, "a username is required"],
+	},
+	name: String,
+	password: {
+		type: String,
+		minLength: 3,
+		required: [true, "a password is required"],
+	},
+});
+
+// userSchema.statics.isUsernameInUse = async function(username){
+// 	if(!username) throw new Error('Invalid username')
+// 	try {
+// 		const user = await this.findOne({username: username});
+// 		if (user) {
+// 			// console.log(user);
+// 			this.message = new Error('nonoooo')
+// 			return false;
+// 		}
+// 	} catch (error) {
+// 		// console.log("method error - isUsernameInUse", error);
+// 		return true;
+// 	}
+// };
 
 userSchema.set("toJSON", {
 	transform: (document, returnedObject) => {
 		(returnedObject.id = returnedObject._id.toString()),
 			delete returnedObject._id,
 			delete returnedObject.__v;
-            delete returnedObject.passwordHash
+		delete returnedObject.password;
 	},
 });
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
---
+++ tests/user_api.test.js
@@ -0,0 +1,70 @@
+const app = require("../app");
+const User = require("../models/user");
+const supertest = require("supertest");
+
+const api = supertest(app);
+
+const initialUsers = [
+	{
+		username: "UncleBob",
+		name: "John smith",
+		password: "password123",
+	},
+];
+
+beforeEach(async () => {
+	await User.deleteMany({});
+	const newUser = new User(initialUsers[0]);
+	await newUser.save();
+}, 10000);
+
+describe("check api methods - /users", () => {
+	test("test post method", async () => {
+		const newUser = {
+			username: "test-username",
+			name: "test-name",
+			password: "test-password",
+		};
+
+		await api
+			.post("/api/users")
+			.send(newUser)
+			.expect(201)
+			.expect("Content-type", /application\/json/);
+
+		const allUsers = await api.get("/api/users");
+		expect(allUsers.body).toHaveLength(initialUsers.length + 1);
+	});
+});
+
+describe("check error handling", () => {
+	describe("username", () => {
+		test("test empty username", async () => {
+			const newUser = {
+				name: "test-name",
+				password: "test-password",
+			};
+
+			const result = await api.post("/api/users").send(newUser).expect(400);
+
+			expect(result.text).toContain(
+				"User validation failed: username: a username is required",
+			);
+		});
+	});
+
+	describe("password", () => {
+		test("test empty password", async () => {
+			const newUser = {
+				username: "test-username",
+				name: "test-name",
+			};
+
+			const result = await api.post("/api/users").send(newUser).expect(400);
+
+			expect(result.text).toContain(
+				"User validation failed: password: a password is required",
+			);
+		});
+	});
+});
