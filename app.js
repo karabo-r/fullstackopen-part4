169d7a8a9269df82ed8970c98d5bba74b3102cce
Karabo <karaborabothata0@gmail.com>
2022-07-29 19:20:44 +0200

    middleware update

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const config = require("./utils/config");
+const errorHandler = require("./utils/errorHandler")
 const mongoose = require('mongoose')
 const BlogRouter = require('./controllers/blog')
-const UserRouter = require('./controllers/user')
+const UserRouter = require('./controllers/user');
 
 mongoose
 .connect(config.MONGO_URI)
@@ -14,8 +15,10 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', BlogRouter)
 app.use('/api/users', UserRouter)
+
 app.get('/',(request, response)=>{
     response.send('hello mom')
 })
 
+app.use(errorHandler)
 module.exports = app
