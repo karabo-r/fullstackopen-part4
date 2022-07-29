e087d2a06b98ca99ffcd351aa168767f96e3b585
Karabo <karaborabothata0@gmail.com>
2022-07-29 16:47:35 +0200

    exercise 4.15 complete

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,23 @@
 const UserRouter = require('express').Router()
 const User = require('../models/user')
+const bcrypt = require('bcrypt')
+
+
+UserRouter.get('/', async (request, response)=>{
+    const AllUsersInDb = await User.find({})
+    response.json(AllUsersInDb)
+})
 
 UserRouter.post('/', async (request, response)=>{
-    const {username, name, passwordHash} = request.body
+    const {username, name, password} = request.body
 
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+    console.log(passwordHash);
     const newUser = new User({
         username,
         name,
-        passwordHash
+        passwordHash,
     })
 
     const savedUser = await newUser.save()
--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
     username: String,
     name: String,
-    passwordHash: String
+    password: String
 })
 
 userSchema.set("toJSON", {
