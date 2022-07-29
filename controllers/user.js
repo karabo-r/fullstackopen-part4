6eb356ce8a41ce3eb832a035749b5371bc13e7c6
Karabo <karaborabothata0@gmail.com>
2022-07-29 19:21:09 +0200

    user api update

--- controllers/user.js
+++ controllers/user.js
@@ -1,27 +1,37 @@
-const UserRouter = require('express').Router()
-const User = require('../models/user')
-const bcrypt = require('bcrypt')
+const UserRouter = require("express").Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+
+UserRouter.get("/", async (request, response) => {
+	const AllUsersInDb = await User.find({});
+	response.json(AllUsersInDb);
+});
+
+UserRouter.post("/", async (request, response, next) => {
+    try {
+        
+        const { username, name } = request.body;
+        let password = request.body.password;
+        let passwordHash = "";
+        if (password) {
+            const saltRounds = 10;
+            passwordHash = await bcrypt.hash(password, saltRounds);
+        }
+    
+        const newUser = new User({
+            username,
+            name,
+            password: passwordHash,
+        });
+    
+        const savedUser = await newUser.save();
+        response.status(201).json(savedUser);
+    } catch (error){
+        // response.status(400).send(error)
+        next(error)
+    }
 
 
-UserRouter.get('/', async (request, response)=>{
-    const AllUsersInDb = await User.find({})
-    response.json(AllUsersInDb)
-})
+});
 
-UserRouter.post('/', async (request, response)=>{
-    const {username, name, password} = request.body
-
-    const saltRounds = 10
-    const passwordHash = await bcrypt.hash(password, saltRounds)
-    console.log(passwordHash);
-    const newUser = new User({
-        username,
-        name,
-        passwordHash,
-    })
-
-    const savedUser = await newUser.save()
-    response.status(201).json(savedUser)
-})
-
-module.exports = UserRouter
+module.exports = UserRouter;
